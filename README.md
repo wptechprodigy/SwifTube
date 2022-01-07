46847b609bbd0a4f47af4f80c6d61e5275ca7bb6
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-01-07 15:32:10 +0100

    add `Constants` file but removed API Key

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# SwifTube 
-
-[![Build Status](https://app.bitrise.io/app/ab19ad206bf554f2/status.svg?token=k06LPJ_QBR9n7UeKRaaXuw&branch=main)](https://app.bitrise.io/app/ab19ad206bf554f2)
+# SwifTube [![Build Status](https://app.bitrise.io/app/ab19ad206bf554f2/status.svg?token=k06LPJ_QBR9n7UeKRaaXuw&branch=main)](https://app.bitrise.io/app/ab19ad206bf554f2)
 
 A video app
--- SwifTube/Commons/Constants.swift
+++ SwifTube/Commons/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Constants {
-    static var API_KEY = "AIzaSyCZzbeIzRhtSK7UO6xugp6ztfcwis1uO-s"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "PLpvpznviFFFI1YpMxb3fQNWKzqvR9BOlv"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
