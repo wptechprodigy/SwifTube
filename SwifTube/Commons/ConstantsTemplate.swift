b5cf8913043f26fc513ceb4b9c776e365842e906
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-01-07 17:00:35 +0100

    ignore `Constants.swift` file

---
+++ SwifTube/Commons/ConstantsTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantsTemplate.swift
+//  SwifTube
+//
+//  Created by waheedCodes on 07/01/2022.
+//
+
+import Foundation
+
+/*
+struct ConstantsTemplate {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(ConstantsTemplate.PLAYLIST_ID)&key=\(ConstantsTemplate.API_KEY)"
+}
+*/
